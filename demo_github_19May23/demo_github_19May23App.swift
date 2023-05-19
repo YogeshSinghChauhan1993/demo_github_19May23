54c0f9f39b1a897131613a36a6e0f30ddaaac8b3
narendraprajapati10 <narendraprajapati10@gmail.com>
2023-05-19 14:00:02 +0530

    Initial Commit

---
+++ demo_github_19May23/demo_github_19May23App.swift
@@ -0,0 +1,17 @@
+//
+//  demo_github_19May23App.swift
+//  demo_github_19May23
+//
+//  Created by WhyQ on 19/05/23.
+//
+
+import SwiftUI
+
+@main
+struct demo_github_19May23App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ demo_github_19May23UITests/demo_github_19May23UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  demo_github_19May23UITestsLaunchTests.swift
+//  demo_github_19May23UITests
+//
+//  Created by WhyQ on 19/05/23.
+//
+
+import XCTest
+
+final class demo_github_19May23UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
